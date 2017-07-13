6ecec28d133918832691f7b887edfc4d4cdd2c7a
ZhuLifeng <820917549@qq.com>
2017-07-14 01:53:41 +0800

    change webpack.config.prod.js for publish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 import './css/index.css';
 import Pagination from './components/Pagination';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 
-let element = document.createElement('div');
-document.body.appendChild(element);
-ReactDOM.render(<Pagination />, element);
+export default Pagination;
+
+//ReactDOM.render(<Pagination />, document.getElementById('root'));
 // registerServiceWorker();
 
